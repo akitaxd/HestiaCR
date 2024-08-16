0bcaeabcaea03c424087521c97f829ddf49c1707
akita <akita@femboy.college>
2024-08-16 14:59:27 +0300

    a little fix again

--- HestiaCR/src/main.rs
+++ HestiaCR/src/main.rs
@@ -15,31 +15,29 @@ fn main() {
     let field_id_3 = game.get_field_id(klass_2,"a","Lcom/craftrise/m9;");
     let field_id_2 = game.get_field_id(klass,"H","Lcom/craftrise/pV;");
     let field_id = game.get_field_id(klass,"cj","Lcom/craftrise/client/S;");
-    let minecraft = game.get_static_object_field(klass,field_id);
     loop {
         unsafe {
-            if GetAsyncKeyState(VK_LBUTTON) != 0 {
-                let mouse_over_object = game.get_object_field(minecraft,field_id_2);
-                let mouse_over_entity = game.get_object_field(mouse_over_object,field_id_3);
-                if mouse_over_entity != 0 {
-                    let mut input = INPUT {
-                        type_: INPUT_MOUSE,
-                        u: std::mem::zeroed(),
-                    };
-                    *input.u.mi_mut() = MOUSEINPUT {
-                        dx: 0,
-                        dy: 0,
-                        mouseData: 0,
-                        dwFlags: MOUSEEVENTF_LEFTDOWN,
-                        time: 0,
-                        dwExtraInfo: 0,
-                    };
-                    SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
-                    input.u.mi_mut().dwFlags = MOUSEEVENTF_LEFTUP;
-                    SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
-                }
-                thread::sleep(Duration::from_millis(100));
+            let minecraft = game.get_static_object_field(klass,field_id);
+            let mouse_over_object = game.get_object_field(minecraft,field_id_2);
+            let mouse_over_entity = game.get_object_field(mouse_over_object,field_id_3);
+            if mouse_over_entity != 0 {
+                let mut input = INPUT {
+                    type_: INPUT_MOUSE,
+                    u: std::mem::zeroed(),
+                };
+                *input.u.mi_mut() = MOUSEINPUT {
+                    dx: 0,
+                    dy: 0,
+                    mouseData: 0,
+                    dwFlags: MOUSEEVENTF_LEFTDOWN,
+                    time: 0,
+                    dwExtraInfo: 0,
+                };
+                SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+                input.u.mi_mut().dwFlags = MOUSEEVENTF_LEFTUP;
+                SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
             }
+            thread::sleep(Duration::from_millis(85));
         }
     }
 
